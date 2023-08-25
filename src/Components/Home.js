1e29318ff6ece6a6704e65128f2c86b56dfb8d3c
Kunalsoni01 <kssoni11111@gmail.com>
2023-08-25 14:21:19 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import data from "./Data";
+
+function Home() {
+  return (
+    <>
+      {data.map(({ id, imgSrc, name, destination, description }) => {
+        return (
+          <>
+            <div key={id} className="card" style={{ width: "18rem;" }}>
+              <img src={imgSrc} className="card-img-top" alt="img" />
+              <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <h5 className="card-title">{destination}</h5>
+                <p className="card-text">{description}</p>
+                <a href={`/Details${name}`} className="btn btn-primary">
+                  konw more
+                </a>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+}
+
+export default Home;
